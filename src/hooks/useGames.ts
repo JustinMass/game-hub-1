cbda842f29d97db731bb38c6b2221065cfbd0327
Justin Massey <justin.massey@auctane.com>
2023-05-28 14:59:46 -0700

    Refactor: Simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,8 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import { GameQuery } from "../App";
-import APIClient, { FetchResponse } from "../services/api-client";
+import { useInfiniteQuery } from '@tanstack/react-query';
+import ms from 'ms';
+import { GameQuery } from '../App';
 import { Platform } from '../hooks/usePlatforms';
+import APIClient, { FetchResponse } from '../services/api-client';
 
 const apiClient = new APIClient<Game>('/games');
 
@@ -17,23 +18,20 @@ export interface Game {
 const useGames = (gameQuery: GameQuery) =>
   useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: ({ pageParam = 1 }) => 
-      apiClient
-        .getAll({
-          params: {
-            genres: gameQuery.genreId,
-            parent_platforms: gameQuery.platformId,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText, 
-            page: pageParam,
-          }
-        }),
-        getNextPageParam: (lastPage, allPages) => {
-          return lastPage.next ? allPages.length + 1 
-          : 
-          undefined;
+    queryFn: ({ pageParam = 1 }) =>
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genreId,
+          parent_platforms: gameQuery.platformId,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+          page: pageParam,
         },
-        staleTime: 24 * 60 * 60 * 1000 // 24 hours
-  })
+      }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
+    staleTime: ms('24h'),
+  });
 
 export default useGames;
