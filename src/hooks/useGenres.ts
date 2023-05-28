ff7296729bf5d8ad2ecd08b9784c275a44754665
Justin Massey <justin.massey@auctane.com>
2023-05-28 14:52:39 -0700

    Refactor: create lookup hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
-import genres from "../data/genres";
-import APIClient from "../services/api-client";
+import { useQuery } from '@tanstack/react-query';
+import genres from '../data/genres';
+import APIClient from '../services/api-client';
 
 const apiClient = new APIClient<Genre>('/genres');
 
@@ -10,10 +10,11 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => useQuery({
-  queryKey: ['genres'],
-  queryFn: apiClient.getAll,
-  staleTime: 24 * 60 * 60 * 1000, //24 hours
-  initialData: { count: genres.length, results: genres }
-})
+const useGenres = () =>
+  useQuery({
+    queryKey: ['genres'],
+    queryFn: apiClient.getAll,
+    staleTime: 24 * 60 * 60 * 1000, //24 hours
+    initialData: genres,
+  });
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
-import platforms from "../data/platforms";
-import APIClient from "../services/api-client";
+import { useQuery } from '@tanstack/react-query';
+import platforms from '../data/platforms';
+import APIClient from '../services/api-client';
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
 
@@ -10,11 +10,12 @@ export interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => useQuery({
+const usePlatforms = () =>
+  useQuery({
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
-        staleTime: 24 * 60 * 60 * 1000, //24 hours
-        initialData: { count: platforms.length, results: platforms}
-})
+    staleTime: 24 * 60 * 60 * 1000, //24 hours
+    initialData: platforms,
+  });
 
 export default usePlatforms;
