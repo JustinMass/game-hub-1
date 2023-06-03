dd71a4b9618798b7d0b6387797351d1cdd7dd92f
Justin Massey <justin.massey@auctane.com>
2023-06-03 13:12:46 -0700

    fix nav bar

--- src/components/GameScreenShots.tsx
+++ src/components/GameScreenShots.tsx
@@ -14,7 +14,7 @@ const GameScreenShots = ({ gameId }: Props) => {
   if (error) throw error;
 
   return (
-    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+    <SimpleGrid marginTop={5} columns={{ base: 1, md: 2 }} spacing={2}>
       {data?.results.map((file) => (
         <Image key={file.id} src={file.image} />
       ))}
